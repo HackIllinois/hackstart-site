5aedbdf9f9fe410145663f575b157d8d82249850
Aniket Gargya <gargya.aniket@gmail.com>
2022-07-29 00:23:40 -0700

    :truck: change event info to about

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Hero from './Hero';
+import About from './About';
 import styles from './styles.module.scss';
 
 const Home: React.FC = () => (
   <div className={styles.home}>
     <Hero />
+    <About />
   </div>
 );
 
