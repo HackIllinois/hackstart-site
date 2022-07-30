7bb1aecbfd4eed3c4f8dec186f0b816cc11d15cb
Aniket Gargya <gargya.aniket@gmail.com>
2022-07-30 00:15:44 -0700

    :speech_balloon: modify faq section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Hero from './Hero';
 import About from './About';
+import FAQ from './FAQ';
 import styles from './styles.module.scss';
 
 const Home: React.FC = () => (
   <main className={styles.home}>
     <Hero />
     <About />
+    <FAQ />
   </main>
 );
 
