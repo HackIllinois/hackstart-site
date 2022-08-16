a10f373452c3aad54e0fdf781704b8917ddff4e1
Aniket Gargya <gargya.aniket@gmail.com>
2022-08-16 00:35:49 -0500

    :lipstick: finish hero styling

--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
+import SOFT_STAR from 'assets/home/shapes/soft_star.svg';
+import ORANGE_ELLIPSES from 'assets/home/shapes/orange_ellipses.svg';
+import BLUE_DIAMOND from 'assets/home/shapes/blue_diamond.svg';
+import PURPLE_X from 'assets/home/shapes/purple_x.svg';
+import PARTY from 'assets/home/shapes/party.svg';
+import WHITE_ELLIPSES from 'assets/home/shapes/white_ellipses.svg';
+
 import clsx from 'clsx';
 import styles from './styles.module.scss';
 
@@ -34,15 +41,31 @@ const AnimatedElement: React.FC = () => {
 const Hero: React.FC = () => {
   return (
     <section className={styles.hero}>
-      <header className={styles.header}>
-        <h4>HackIllinois presents</h4>
-        <h1>
+      <div className={styles.leftColumn}>
+        <img className={clsx(styles.softStar1, "shape")} src={SOFT_STAR} alt="" />
+        <img className={clsx(styles.orangeEllipses, "shape")} src={ORANGE_ELLIPSES} alt="" />
+        <img className={clsx(styles.blueDiamond1, "shape")} src={BLUE_DIAMOND} alt="" />
+        <img className={clsx(styles.purpleX, "shape")} src={PURPLE_X} alt="" />
+        <img className={clsx(styles.softStar2, "shape")} src={SOFT_STAR} alt="" />
+      </div>
+
+      <header className={styles.centerColumn}>
+        <h4 className="mb-small">HackIllinois presents</h4>
+        <h1 className="mb-small">
           Hack
           <AnimatedElement />
         </h1>
-        <h3>September 17, 2022</h3>
-        <a className={styles.applyButton} href="/">Apply Now</a>
+        <h3 className="mb-small">September 17, 2022</h3>
+        <div className={styles.applyButton}>Apply Now</div>
       </header>
+
+      <div className={styles.rightColumn}>
+        <img className={clsx(styles.softStar3, "shape")} src={SOFT_STAR} alt="" />
+        <img className={clsx(styles.party, "shape")} src={PARTY} alt="" />
+        <img className={clsx(styles.whiteEllipses, "shape")} src={WHITE_ELLIPSES} alt="" />
+        <img className={clsx(styles.blueDiamond2, "shape")} src={BLUE_DIAMOND} alt="" />
+        <img className={clsx(styles.softStar4, "shape")} src={SOFT_STAR} alt="" />
+      </div>
     </section>
   );
 };
