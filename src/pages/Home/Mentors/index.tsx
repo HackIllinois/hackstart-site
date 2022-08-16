5487194a490169e9fc0a973d9747e292b1be6b7c
Aniket Gargya <gargya.aniket@gmail.com>
2022-08-16 15:55:39 -0500

    :lipstick: add mentors shapes

--- src/pages/Home/Mentors/index.tsx
+++ src/pages/Home/Mentors/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-// import SOFT_STAR from 'assets/home/shapes/soft_star.svg';
-// import ORANGE_ELLIPSES from 'assets/home/shapes/orange_ellipses.svg';
-// import BLUE_DIAMOND from 'assets/home/shapes/blue_diamond.svg';
-// import PURPLE_X from 'assets/home/shapes/purple_x.svg';
-// import PARTY from 'assets/home/shapes/party.svg';
-// import WHITE_ELLIPSES from 'assets/home/shapes/white_ellipses.svg';
+import LAME_STAR from 'assets/home/shapes/lame_star.svg';
+import SOFT_STAR from 'assets/home/shapes/soft_star.svg';
+import PARTY from 'assets/home/shapes/party.svg';
+import PURPLE_ELLIPSES from 'assets/home/shapes/purple_ellipses.svg';
+import RED_ELLIPSES from 'assets/home/shapes/red_ellipses.svg';
+import BLUE_ELLIPSES from 'assets/home/shapes/blue_ellipses.svg';
+import ORANGE_X from 'assets/home/shapes/orange_x.svg';
 
 import clsx from 'clsx';
 import styles from './styles.module.scss';
@@ -24,6 +25,19 @@ const Mentors: React.FC = () => (
         </span>
       </div>
     ))}
+
+    <img className={clsx(styles.lameStar1, styles.shape, "shape")} src={LAME_STAR} alt="" />
+    <img className={clsx(styles.softStar, styles.shape, "shape")} src={SOFT_STAR} alt="" />
+    <img className={clsx(styles.purpleEllipses, styles.shape, "shape")} src={PURPLE_ELLIPSES} alt="" />
+    <img className={clsx(styles.orangeX, styles.shape, "shape")} src={ORANGE_X} alt="" />
+    <img className={clsx(styles.lameStar2, styles.shape, "shape")} src={LAME_STAR} alt="" />
+
+    <img className={clsx(styles.lameStar3, styles.shape, "shape")} src={LAME_STAR} alt="" />
+    <img className={clsx(styles.blueEllipses, styles.shape, "shape")} src={BLUE_ELLIPSES} alt="" />
+    <img className={clsx(styles.lameStar4, styles.shape, "shape")} src={LAME_STAR} alt="" />
+    <img className={clsx(styles.party, styles.shape, "shape")} src={PARTY} alt="" />
+    <img className={clsx(styles.lameStar5, styles.shape, "shape")} src={LAME_STAR} alt="" />
+    <img className={clsx(styles.redEllipses, styles.shape, "shape")} src={RED_ELLIPSES} alt="" />
   </section>
 );
 
