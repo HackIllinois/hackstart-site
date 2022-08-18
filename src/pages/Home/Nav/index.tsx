c39d06abaec3d62d71389f103efda412addee040
Aniket Gargya <gargya.aniket@gmail.com>
2022-08-17 23:21:42 -0500

    :sparkles: add hover color to navbar links

--- src/pages/Home/Nav/index.tsx
+++ src/pages/Home/Nav/index.tsx
@@ -7,16 +7,16 @@ import LOGO from 'assets/home/shapes/hack_logo.svg';
 const Nav: React.FC = () => (
   <nav>
     <img src={LOGO} alt="HackIllinois Logo" className={styles.logo} />
-    <a href="#" className={styles.link}>
+    <a href="#" className={clsx(styles.link, styles.home)}>
       <span>Home</span>
     </a>
-    <a href="#about" className={styles.link}>
+    <a href="#about" className={clsx(styles.link, styles.about)}>
       <span>About</span>
     </a>
-    <a href="#faq" className={styles.link}>
+    <a href="#faq" className={clsx(styles.link, styles.faq)}>
       <span>FAQ</span>
     </a>
-    <a href="#mentors" className={styles.link}>
+    <a href="#mentors" className={clsx(styles.link, styles.mentors)}>
       <span>Mentors</span>
     </a>
   </nav>
